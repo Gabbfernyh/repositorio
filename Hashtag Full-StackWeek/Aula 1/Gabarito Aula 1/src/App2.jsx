d3fc4a8e3512ceee126019647ad89997b85006bb
marcos gabriel <bielmgfv.python@gmail.com>
2025-02-12 20:20:34 -0300

    primeiro commit

---
+++ Hashtag Full-StackWeek/Aula 1/Gabarito Aula 1/src/App2.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const App2 = () => {
+    return (
+        <div>App2</div>
+    )
+}
+
+export default App2
+
+// Rafce
+
+// Arrow Function
+// const App2 = () => <h1>Ola, mundo!</h1>;
+
+// Nomeação de componente
+// PascalCase
+
+// Nomeação de Variavel, Função...
+// camelCaseCorcundaDoCamelo
+
+// expot default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// self closing tag
+// <Header> </Header>
+// <Header />
